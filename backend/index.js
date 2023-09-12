7e0c072acb6c13d25712671a73371cf91da0b7c6
RaniKamna <kamna.a139@gmail.com>
2023-09-12 12:30:35 +0530

    index file updated

--- backend/index.js
+++ backend/index.js
@@ -22,12 +22,12 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin (for development)
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin (for development)
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
